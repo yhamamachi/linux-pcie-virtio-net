b447b2c57e3c444d61a4badfc0bb2a39becc4c39
Yuya Hamamachi <yuya.hamamachi.sx@renesas.com>
2024-08-05 17:08:50 +0900

    Revert "virtio-pci: Add MSI support"
    This reverts commit 1019386f9ca9f9eaf36b508e6669831e985ec221.

--- drivers/virtio/virtio_pci_common.h
+++ drivers/virtio/virtio_pci_common.h
@@ -61,7 +61,7 @@ struct virtio_pci_device {
 	/* array of all queues for house-keeping */
 	struct virtio_pci_vq_info **vqs;
 
-	/* MSI/MSI-X support */
+	/* MSI-X support */
 	int msix_enabled;
 	int intx_enabled;
 	cpumask_var_t *msix_affinity_masks;
